1ab8755740a1b2da03c90adebb0421c1e713a06b
kenny yuen <kennyyuen@kennys-macbook-pro.local>
2018-09-08 00:37:44 +0800

    Initial Commit

---
+++ Helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Helloworld
+//
+//  Created by kenny yuen on 8/9/2018.
+//  Copyright © 2018年 kenny yuen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
